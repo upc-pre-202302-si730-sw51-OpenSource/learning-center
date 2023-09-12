cd183ba1d36d23a1718d0a71780c26230e9c45dd
diego04s03 <u20201c334@upc.edu.pe>
2023-09-12 09:06:22 -0500

    feat(students): added students component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
+import { StudentsComponent } from './learning/pages/students/students.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentsComponent
   ],
   imports: [
     BrowserModule,
